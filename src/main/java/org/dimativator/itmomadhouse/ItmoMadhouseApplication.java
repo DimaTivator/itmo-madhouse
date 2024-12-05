ef17d9f2275a675f507f6a9acf3a20ded35a2371
DimaTivator <andriianov164@gmail.com>
2024-12-05 13:35:09 +0300

    init

---
+++ src/main/java/org/dimativator/itmomadhouse/ItmoMadhouseApplication.java
@@ -0,0 +1,13 @@
+package org.dimativator.itmomadhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmoMadhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItmoMadhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dimativator/itmomadhouse/ItmoMadhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dimativator.itmomadhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmoMadhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
